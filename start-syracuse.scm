4809fa2e6ba3acdba20a8fc066731dccecc64d5f
Damien MATTEI <damien.mattei@gmail.com>
2018-10-28 09:02:14 +0100

    created starter program for syracuse
    added start-syracuse.scm
    added and modified some comments in syracuse.scm

---
+++ start-syracuse.scm
@@ -0,0 +1,19 @@
+#lang racket
+
+
+
+(include "syntactic-sugar.scm")
+(include "debug.scm")
+(include "list.scm")
+(include "operation.scm")
+(include "symbolic.scm")
+(include "simplify.scm")
+(include "racket/escape-char-racket-scheme.scm")
+(include "display.scm")
+(include "binary-arithmetic.scm")
+(include "for.scm")
+(include "pair.scm")
+
+(require racket/format)
+
+(include "syracuse.scm")
