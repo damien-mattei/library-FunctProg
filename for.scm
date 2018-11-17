1ab2114716d89846aee2b9af2487c0ee10c9249e
Damien MATTEI <damien.mattei@gmail.com>
2018-11-17 15:34:21 +0100

    added macro for-next

--- for.scm
+++ for.scm
@@ -77,3 +77,80 @@
        (when (<= i to)
 	     b1 ...
 	     (loop (+ i step)))))))
+
+
+;; (for-next k = 0 to 10  (display k) (newline))
+;; 0
+;; 1
+;; 2
+;; 3
+;; 4
+;; 5
+;; 6
+;; 7
+;; 8
+;; 9
+;; 10
+;; > (for-next k = 0 to 5 step 2 (display k) (newline))
+;; 0
+;; 2
+;; 4
+;; > (for-next (k = 0 to 5 step 2) (display k) (newline))
+;; 0
+;; 2
+;; 4
+;; > (for-next (k = 0 to 5) (display k) (newline))
+;; 0
+;; 1
+;; 2
+;; 3
+;; 4
+;; 5
+;; > 
+
+(define-syntax for-next
+  (syntax-rules (= to step)
+
+
+    ;; The patterns are matched top down (the one with step must be before others patterns)
+    ((for-next i = start to finish step inc b1 ...)
+     
+     (let loop ((i start))
+       (when (<= i finish)
+	     b1 ...
+	     (loop (+ i inc)))))
+
+    
+    ((for-next i = start to finish b1 ...)
+
+     (let loop ((i start))
+       (when (<= i finish)
+	     b1 ...
+	     (loop (incf i)))))
+    
+   
+    
+    ((for-next (i = start to finish) b1 ...)
+
+     (let loop ((i start))
+       (when (<= i finish)
+	     b1 ...
+	     (loop (incf i)))))
+    
+    ((for-next (i = start to finish step inc) b1 ...)
+     
+     (let loop ((i start))
+       (when (<= i finish)
+	     b1 ...
+	     (loop (+ i inc)))))))
+
+
+(define-syntax for-next-step
+  (syntax-rules (= to step)
+    
+    ((for-next-step i = start to finish step inc b1 ...)
+     
+     (let loop ((i start))
+       (when (<= i finish)
+	     b1 ...
+	     (loop (+ i inc)))))))
