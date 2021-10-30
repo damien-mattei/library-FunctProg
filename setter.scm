c330d6500ef15597e391577af93ae2ddadffd899
Damien MATTEI <damien.mattei@gmail.com>
2021-10-30 23:30:44 +0200

    removed files

--- setter.scm
+++
@@ -1,8 +0,0 @@
-;; array setter are in array.scm and guile/array.scm
-(define-syntax <-
-  (syntax-rules ()
-     
-    ((_ var expr)  ;; (if (defined? var)
-     ;;     (begin
-     ;; 	 (display "← : variable set!") (newline)
-     (set! var expr))))
