d39837ddbcb4e46faee2e06ce4df65e46d48d2a0
Damien MATTEI <damien.mattei@gmail.com>
2021-12-09 16:09:56 +0100

    array made of vectors at multiple levels

--- array.scm
+++ array.scm
@@ -1,5 +1,24 @@
 ;; arrays
 
+;; This file is part of Scheme+
+
+;; Copyright 2021 Damien MATTEI
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 ;; TODO : make a version vector with resizable arrays using classes
 ;; cf: https://www.gnu.org/software/guile/manual/html_node/GOOPS.html
 
