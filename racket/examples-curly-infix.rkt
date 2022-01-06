cda52c4d3d34ed9b06a81f18e54d0291ab4d964a
Damien MATTEI <damien.mattei@gmail.com>
2022-01-07 00:04:18 +0100

    function in required file

--- racket/examples-curly-infix.rkt
+++ racket/examples-curly-infix.rkt
@@ -1,6 +1,6 @@
 #lang reader "SRFI-105.rkt"
 
-(require "examples-curly-infix2.rkt")
+;;(require "examples-curly-infix2.rkt")
 
 (- (+ 3 3)
    {2 + 2})
@@ -9,16 +9,15 @@
 
 {5 + 2}
 
+(require "examples-curly-infix2.rkt")
+
 (define (fibonacci n)
   (if (< n 2)
       n
       (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
 (fibonacci 7)
 
-(define (fib n)
-  (if {n < 2}
-      n
-      {(fib {n - 1}) + (fib {n - 2})} ))
+
 
 (fib 11)
 
